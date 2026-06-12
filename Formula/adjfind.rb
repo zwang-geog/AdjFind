ff832e9cfb88ce5cc5f0570555dc09c3c805632e
zwang-geog <zwang.geog@gmail.com>
2026-06-12 09:33:37 -0700

    update homebrew formula to reflect the latest version

--- Formula/adjfind.rb
+++ Formula/adjfind.rb
@@ -1,8 +1,8 @@
 class Adjfind < Formula
   desc "C++ program with specialized path finding algorithms for adjacency/proximity"
   homepage "https://github.com/zwang-geog/AdjFind"
-  url "https://github.com/zwang-geog/AdjFind/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "861fb40762edaafd9a014be4c306c3e47f7c7a2d6d90523b8d4cdc83c67e9ce1"
+  url "https://github.com/zwang-geog/AdjFind/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "e5867d5c90080b64bbf8afd5351442f987df0047ade300604d713774ccb843cc"
   license "MIT"
 
   depends_on "cmake" => :build
